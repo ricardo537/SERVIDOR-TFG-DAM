36a94f913949a51619dc758f50c4ac30fba99650
arteaga3193 <cartsan31@gmail.com>
2025-04-24 18:48:41 +0200

    servidor tfg dam

---
+++ bolas/src/main/java/com/bolas/bolas/BolasApplication.java
@@ -0,0 +1,13 @@
+package com.bolas.bolas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolasApplication.class, args);
+	}
+
+}
---
+++ bolas/src/test/java/com/bolas/bolas/BolasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolas.bolas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
